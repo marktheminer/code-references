68a18f6bae8b1caf8c2ea752a29cf77eecb242ac
GitHub Action <action@github.com>
2022-01-07 19:33:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/marktheminer/codereferences/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.marktheminer.codereferences.services
+
+import com.github.marktheminer.codereferences.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
